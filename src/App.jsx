5c80e1b3371be08540bcd015a91a45f7284705c7
roman.pohribniak <roman.pohribniak@gmail.com>
2023-02-04 22:02:34 +0200

    add api load

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combinedReducer } from "./reducers/combinedReducer";
 import Cart from "./components/Cart/Cart";
 import { Provider } from "react-redux";
+import DishesList from "./components/DishList/DishesList";
 
 const store = configureStore({reducer:combinedReducer})
 
@@ -9,6 +10,7 @@ export default function App(){
     return(
         <Provider store={store}>
             <Cart />
+            <DishesList/>
         </Provider>
     )
 }
---
+++ src/components/DishList/DishesList.jsx
@@ -0,0 +1,30 @@
+import Dish from "./Dish";
+
+
+import { useEffect, useState } from "react"
+
+export default function DishesList() {
+    const [mealDB,updateDB] = useState([]);
+    useEffect(()=>{
+        const arr = [];
+            (async ()=>{    
+                while(arr.length < 9){
+                const {meals} = await (await fetch("https://www.themealdb.com/api/json/v1/1/random.php")).json();
+                    if(meals){
+                        if(!arr.find(meal=>meal.idMeal === meals[0].idMeal)){
+                            arr.push(meals[0]);
+                        }
+                    }
+                }
+                updateDB(arr);
+            })()
+    },[])
+    if(!mealDB.length){
+        return "loading..."
+    }
+    return(<section className="dish">
+        {mealDB.map(meal=>{
+            return <Dish key={meal.idMeal} dish={meal}/>
+        })}
+        </section>)
+}
