29d97d8b525af9b01bfba03cbd1c871c38a21098
roman.pohribniak <roman.pohribniak@gmail.com>
2023-02-05 11:47:10 +0200

    SortCart fix

--- src/components/Cart/SortCart.jsx
+++ src/components/Cart/SortCart.jsx
@@ -7,27 +7,27 @@ import Typography from '@mui/material/Typography';
 export default function SortCart({cart,isCategoryVisible,removeProduct}){
     return (
         <Stack direction="row" sx={{width:300,flexWrap:"wrap"}}>
-            {/*{!isCategoryVisible ?*/}
-            {/*        cart.products.map((product,index)=>{*/}
-            {/*            return <Chip*/}
-            {/*                        key={index}*/}
-            {/*                        variant="outlined"*/}
-            {/*                        label={product.label}*/}
-            {/*                        onDelete={()=>removeProduct(product)}/>*/}
-            {/*                    }):*/}
-            {/*        cartHandlers[0](cart.products).map((category,ind)=>{*/}
-            {/*            return <section key={ind}>*/}
-            {/*                        <Typography variant='h5'>{category}</Typography>*/}
-            {/*                        <Stack sx={{width:300}}  direction="row">*/}
-            {/*                        {cartHandlers[1](cart.products,category).map((product,index)=>{*/}
-            {/*                                return <Chip*/}
-            {/*                                            key={index}*/}
-            {/*                                            variant="outlined"*/}
-            {/*                                            label={product.label}*/}
-            {/*                                            onDelete={()=>removeProduct(product)}/>*/}
-            {/*                                        })}*/}
-            {/*                        </Stack>*/}
-            {/*                    </section>*/}
-            {/*    })}*/}
+             {!isCategoryVisible ? 
+                     cart.products.map((product,index)=>{ 
+                         return <Chip 
+                                     key={index} 
+                                     variant="outlined" 
+                                     label={product.label} 
+                                     onDelete={()=>removeProduct(product)}/> 
+                                 }): 
+                     cartHandlers[0](cart.products).map((category,ind)=>{ 
+                         return <section key={ind}> 
+                                     <Typography variant='h5'>{category}</Typography> 
+                                     <Stack sx={{width:300}}  direction="row"> 
+                                     {cartHandlers[1](cart.products,category).map((product,index)=>{ 
+                                             return <Chip 
+                                                         key={index} 
+                                                         variant="outlined" 
+                                                         label={product.label} 
+                                                         onDelete={()=>removeProduct(product)}/> 
+                                                     })} 
+                                     </Stack> 
+                                 </section> 
+                 })}
         </Stack>)
 }
