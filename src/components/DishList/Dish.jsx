a328084730103e4595e3c3cdc8821dec7ec225cb
BohdanD <alphatech664@gmail.com>
2023-02-05 23:21:30 +0200

    new styles for dish container

--- src/components/DishList/Dish.jsx
+++ src/components/DishList/Dish.jsx
@@ -18,7 +18,7 @@ export default function Dish({dish}) {
         <div className="dish__meal">
             <div className="dish__meal--img" style={{backgroundImage:`url(${dish.strMealThumb})`}}/>
             <div className="dish__meal--description">
-                <Typography className="dish__meal--title" variant="h5" onClick={()=>{addDish(dish);openModal(true)}}>{dish.strMeal}</Typography>
+                <Typography style={{marginTop: '20px'}} className="dish__meal--title" variant="h5" onClick={()=>{addDish(dish);openModal(true)}}>{dish.strMeal}</Typography>
                 <ul className="dish__meal--products" style={{listStyleType: 'disc', width: 'max-content', marginTop: 'auto', marginBottom: 'auto'}}>
                     {shortCut.map(ingr=>{
                         return <li key={ingr}>{ingr}</li>
