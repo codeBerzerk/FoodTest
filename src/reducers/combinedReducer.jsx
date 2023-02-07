b1fe8664c8972cd97f026ba1e629706989f61029
roman.pohribniak <roman.pohribniak@gmail.com>
2023-02-07 18:35:54 +0200

    deploy

--- src/reducers/combinedReducer.jsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import { cart } from "./cartReducer";
-import { dish } from "./dishReducer";
-
-export const combinedReducer = combineReducers({
-    cart:cart,
-    dish:dish,
-})
